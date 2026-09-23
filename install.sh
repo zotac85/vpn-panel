9a57546468c7169b3326684d78169506ec2d46f0
zotac85 <zotac1985@gmail.com>
2026-09-23 06:51:18 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,9 +56,9 @@ if [ ! -f /etc/UDPCustom/start.txt ] || ! grep -q "sponsors_list" /etc/UDPCustom
     curl -sf -o /etc/UDPCustom/start.txt "$REPO_URL/configs/start.txt"
 fi
 
-
 # channels.txt — список каналов
 if [ ! -f /etc/UDPCustom/channels.txt ] || [ ! -s /etc/UDPCustom/channels.txt ]; then
+    [ -f /etc/UDPCustom/channels.txt ] && cp /etc/UDPCustom/channels.txt /etc/UDPCustom/channels.txt.bak
     curl -sf -o /etc/UDPCustom/channels.txt "$REPO_URL/configs/channels.txt"
 fi
 
